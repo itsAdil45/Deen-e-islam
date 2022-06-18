21066dcf4c4cb8c56d11e3bf8fc1854b2c46c4fd
itsAdil45 <sheikhadil086@gmail.com>
2022-06-18 21:36:23 +0500

    push notification installed

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Prayers from "./Screens/Prayers";
 import Home from "./Screens/Home";
 import Duas from "./Screens/Duas";
@@ -9,9 +9,59 @@ import DuaTranslation from "./Screens/DuaTranslation";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { initializeApp } from 'firebase/app';
+import * as Notifications from 'expo-notifications';
 const stack = createStackNavigator();
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+  const a = 1
+  const b = 1
+  //  useEffect(async () => await schedulePushNotification(), [])
+  // useEffect(() =>
+  //   Notifications.scheduleNotificationAsync({
+  //     content: {
+  //       title: "Namaz Time 📬",
+  //       body: 'Ao namaz ki taraf',
+
+  //     },
+  //     trigger: {
+  //       seconds: 12,
+  //       repeats: true
+
+  //     },
+  //   })
+  //   , [0])
+  useEffect(() => {
+
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+
+
+
   const firebaseConfig = {
     apiKey: "AIzaSyCPEMslkLZtRJPcTYQxqQtkY0yggqOolmo",
     authDomain: "deen-e-islam-f8024.firebaseapp.com",
@@ -40,6 +90,36 @@ export default function App() {
 
 
   )
+}
+
+
+function schedulePushNotification() {
+  Notifications.scheduleNotificationAsync({
+    content: {
+      title: "Namaz Time 📬",
+      body: 'Ao namaz ki taraf',
+
+    },
+    trigger: {
+      seconds: 2,
+      repeats: true
+
+    },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
 }
 
 const styles = StyleSheet.create({
